f7fc318631c4f26eb08b22857ac74a9584e827ed
yankkvx <yankkvx@gmail.com>
2025-01-04 23:00:20 +0300

    feat: Create serializers for Server, Channel, and Category models
    Created serializersto convert data. Added custom logic to retrieve
    'num_members' and conditionally include it in the response.

---
+++ backend/server/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import Server, Category, Channel
+
+
+# Serializer for Channel model, converting it into JSON format.
+class ChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Channel
+        fields = '__all__'
+
+
+# Serializer for Channel model, converting it into JSON format.
+class ServerSerializer(serializers.ModelSerializer):
+    server_channel = ChannelSerializer(many=True)
+    num_members = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Server
+        fields = '__all__'
+
+    # Custom method to retrieve the number of members in a server.
+    def get_num_members(self, obj):
+        if hasattr(obj, 'num_members'):
+            return obj.num_members
+        return None
+
+    def to_representation(self, instance):
+        # Calls the parent class method for basic serialization
+        data = super().to_representation(instance)
+        # Retrieves 'num_members' from the context
+        num_members = self.context.get('num_members')
+        # If num_members is not in the context, remove it from the serialized data.
+        if not num_members:
+            data.pop('num_members', None)
+        # Returns the serialized data.
+        return data
