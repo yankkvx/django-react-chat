ab1b92bed295e1d859d64ed80942b5a45abfceaa
yankkvx <yankkvx@gmail.com>
2025-01-29 19:37:45 +0300

    feat: enhance WebSocket consumer for real time chat
    - Replace WebsocketConsumer with JsonWebsocketConsumer
    - Implement real-time messaging with channel groups
    - Retrieve channel_id from WebSocket URL
    - Associate messages with Communication and store them in the database
    - Broadcast messages to all clients in the channel
    - Handle WebSocket connection, message reception and disconnection

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,14 +1,70 @@
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import JsonWebsocketConsumer
+from asgiref.sync import async_to_sync
+from .models import Communication, Message
+from django.contrib.auth import get_user_model
 
 
-class MyConsumer(WebsocketConsumer):
+User = get_user_model()
+
+
+class ChatConsumer(JsonWebsocketConsumer):
+    """
+    This class handles WebSocket connettions for chat application.
+    It manages connections, message reception and disconnections
+    """
+
+    def __init__(self, *args, **kwargs):
+        # Initialize the consumer, setting up channel_id and user
+        super().__init__(*args, **kwargs)
+        self.channel_id = None
+        self.user = None
+
     def connect(self):
-        self.accept()
-        # self.close()
+        self.accept()  # Accept WebSocket connection
+        # Extract channel id form the URL
+        self.channel_id = self.scope['url_route']['kwargs']['channelId']
+        # Set the user(currently hardcoded to user with ID 1 for testing)
+        self.user = User.objects.get(id=1)
 
-    def receive(self, text_data=None, bytes_data=None):
-        self.send(text_data=text_data)
-        # self.close()
+        # Add WEbSocket connection to the group for real time communication
+        async_to_sync(self.channel_layer.group_add)(
+            self.channel_id,
+            self.channel_name,
+        )
+
+    def receive_json(self, text_data):
+        channel_id = self.channel_id  # Get the current channel id
+        sender = self.user  # Get the sender user
+        # Extract the message content from the data
+        content = text_data['message']
+
+        # Get or create a communication object for the channel
+        communication, _ = Communication.objects.get_or_create(
+            channel_id=channel_id)
+
+        # Create a new message object associated with Communication
+        new_message = Message.objects.create(
+            communication=communication, sender=sender, content=content)
+
+        # Send the new message to all clients in the group for real-time updates
+        async_to_sync(self.channel_layer.group_send)(
+            self.channel_id,
+            {
+                'type': 'chat.message',
+                'new_message': {
+                    'id': new_message.id,
+                    'sender': new_message.sender.username,
+                    'content': new_message.content,
+                    'timestamp': new_message.timestamp.isoformat()
+                },
+            },
+        )
+
+    def chat_message(self, event):
+        self.send_json(event)  # Send the event data as JSON to the WebSocket
 
     def disconnect(self, close_code):
-        pass
+        # Remove the WebSocket connection from the group when the communication is closed
+        async_to_sync(self.channel_layer.group_discard)(
+            self.channel_id, self.channel_name)
+        super().disconnect(close_code)  # Disconnect the WebSocket
