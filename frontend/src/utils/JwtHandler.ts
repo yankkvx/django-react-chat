837eb91c49a34cb2a5b44aee68916e2783d83838
yankkvx <yankkvx@gmail.com>
2025-02-10 21:48:28 +0300

    feat: add JWT token refresh handling in jwtHandler
    - Added logic to handle token expiration status 401/403 by attempting to
      refresh the token
    - Integrated useAuthService to call logout on token invalidation
    - Configured axios to send cookies with every request globally using
      axios.defaults.withCredentials = true
    - Improved error handling with automatic navigation to the login page on
      failure

--- frontend/src/utils/JwtHandler.ts
+++ frontend/src/utils/JwtHandler.ts
@@ -1,20 +1,43 @@
 import axios, { AxiosInstance } from "axios";
 import { useNavigate } from "react-router";
+import { useAuthService } from "../services/AuthServices";
 
+// Enable sending cookies with every request globally
+axios.defaults.withCredentials = true;
+// Function to create an axios instance with JWT handling logic
 const jwtHandler = (): AxiosInstance => {
-    const navigate = useNavigate();
     const jwtAxios = axios.create({ baseURL: "http://127.0.0.1:8000" });
+    const navigate = useNavigate();
+    const { logout } = useAuthService();
 
+    // Intercepting the response for token expiration or invalidation
     jwtAxios.interceptors.response.use(
         (response) => {
             return response;
         },
         async (error) => {
             const request = error.config;
-            if (error.response?.status === 403) {
-                navigate("/login");
+            if (
+                error.response?.status === 401 ||
+                error.response?.status === 403
+            ) {
+                try {
+                    // Request a new access token using the refresh token
+                    const response = await axios.post(
+                        "http://127.0.0.1:8000/api/token/refresh/"
+                    );
+                    if (response.status == 200) {
+                        // restart the original request with a new token
+                        return jwtAxios(request);
+                    }
+                } catch (refreshError) {
+                    // if the update failed call logout
+                    logout();
+                    const goLogin = () => navigate("/login");
+                    goLogin();
+                    return Promise.reject(error);
+                }
             }
-            return Promise.reject(error);
         }
     );
     return jwtAxios;
