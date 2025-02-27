979de1347e769577c5c0297b18980913e02952dc
yankkvx <yankkvx@gmail.com>
2025-02-27 15:50:48 +0300

    feat: add server management functions and refactor ServerServices
    - Added getServer function to fetch a specific server by ID
    - Added editServer function to update a server's information
    - Added createChannel and deleteChannel functions for channel management
    - Refactored ServerServices to include new functionalities and improved
      structure

--- frontend/src/services/ServerServices.ts
+++ frontend/src/services/ServerServices.ts
@@ -3,6 +3,18 @@ import { MAIN_URL } from "../api-config";
 import { useCallback } from "react";
 
 const useServerService = () => {
+    const getServer = useCallback(async (serverId: number) => {
+        try {
+            const response = await axios.get(
+                `${MAIN_URL}/servers/${serverId}/`
+            );
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }, []);
+
     const createServer = async (formData: FormData) => {
         try {
             const response = await axios.post(
@@ -11,6 +23,20 @@ const useServerService = () => {
                 {
                     withCredentials: true,
                 }
+            );
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    const editServer = async (serverId: number, formData: FormData) => {
+        try {
+            const response = await axios.put(
+                `${MAIN_URL}/servers/${serverId}/edit/`,
+                formData,
+                { withCredentials: true }
             );
             return response.data;
         } catch (error) {
@@ -47,7 +73,44 @@ const useServerService = () => {
         }
     };
 
-    return { createServer, getCategories, createCategory };
+    const createChannel = async (formData: FormData) => {
+        try {
+            const response = await axios.post(
+                `${MAIN_URL}/servers/channel/`,
+                formData,
+                {
+                    withCredentials: true,
+                }
+            );
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    const deleteChannel = async (serverId: number) => {
+        try {
+            const response = await axios.delete(
+                `${MAIN_URL}/servers/channel/${serverId}/delete/`,
+                { withCredentials: true }
+            );
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    return {
+        getServer,
+        createServer,
+        editServer,
+        getCategories,
+        createCategory,
+        createChannel,
+        deleteChannel,
+    };
 };
 
 export default useServerService;
